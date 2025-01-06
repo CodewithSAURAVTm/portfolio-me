64db2865083f08dbf4c3cdc896f1b340990c2751
SAURAV KUMAR <kumarsaurav1287@gmail.com>
2025-01-06 09:15:21 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const sr= ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 200,
+    })
+
+    sr.reveal('.home-content')
+    sr.reveal('.home-img',{delay:200})
+    sr.reveal('.about-content',{origin:'right'})
+    sr.reveal('.about-img',{origin:'left'})
+    sr.reveal('.project',{origin:'left'})
+    sr.reveal('.education',{origin:'right'})
+    sr.reveal('.services',{interval:200})
+    sr.reveal('.contact',{delay:200})
